fff68552dc9c3e471eeb6fa9db498e0761b0c2ac
Akriti <guptaakriti252@gmail.com>
2026-06-24 00:11:30 +0530

    Use MongoDB env variable

--- server/config/db.js
+++ server/config/db.js
@@ -2,14 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb://akriti:akriti123@ac-iopwbrf-shard-00-00.5djqvv0.mongodb.net:27017,ac-iopwbrf-shard-00-01.5djqvv0.mongodb.net:27017,ac-iopwbrf-shard-00-02.5djqvv0.mongodb.net:27017/collabcode?ssl=true&replicaSet=atlas-e6nrq2-shard-0&authSource=admin&retryWrites=true&w=majority"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected");
   } catch (err) {
     console.log("Mongo Error:");
-  console.log(err);
+    console.log(err);
   }
 };
 
